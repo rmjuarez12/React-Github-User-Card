715c6ad4527b15f406e73bcb8e854cbaa5277ac3
rmjuarez12 <rmjuarez12@gmail.com>
2020-10-31 01:11:10 -0400

    Fixed a few bugs with form

--- user-card/src/components/UserForm.js
+++ user-card/src/components/UserForm.js
@@ -14,7 +14,8 @@ export default class UserForm extends Component {
   // Function to handle the onChange of the text field
   handleChange = (e) => {
     this.setState({
-      name: e.target.value
+      name: e.target.value,
+      disableBtn: false
     })
   }
 
@@ -23,9 +24,28 @@ export default class UserForm extends Component {
     e.preventDefault();
 
     if(this.state.name === "") {
-      return;
+      const errorAnim = gsap.timeline({ repeat: 0, repeatDelay: 0 });
+      errorAnim.to(".search-form", { x: -50, duration: 0.2 });
+      errorAnim.to(".search-form", { x: 50, duration: 0.2 });
+      errorAnim.to(".search-form", { x: -20, duration: 0.2 });
+      errorAnim.to(".search-form", { x: 20, duration: 0.2 });
+      errorAnim.to(".search-form", { x: 0, duration: 0.2 });
+
+      this.setState({
+        disableBtn: true,
+      })
+
+      setTimeout(() => {
+        this.setState({
+          disableBtn: false,
+        })
+      }, 1000)
     } else {
       this.props.getNewUserData(this.state.name);
+
+      this.setState({
+        name: ""
+      })
     }
   }
   
@@ -33,9 +53,9 @@ export default class UserForm extends Component {
     return (
       <div className="search-form">
         <form onSubmit={this.handleSubmit}>
-          <input type="text" name="name" placeholder="Search Github User..." value={this.state.name} onChange={this.handleChange} />
+          <input type="text" name="name" placeholder="Search Github User..." value={this.state.name} onChange={this.handleChange} disabled={this.state.disableBtn} />
 
-          <input type="submit" value="Search" />
+          <input type="submit" value="Search" disabled={this.state.disableBtn} />
         </form>
       </div>
     )
